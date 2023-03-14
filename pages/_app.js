7ffb3055bcac24074e7b0b09e454a98c600b29d1
Michael O'Dell <michaelodell660@gmail.com>
2023-03-14 12:27:54 -0700

    new favicon

--- pages/_app.js
+++ pages/_app.js
@@ -5,19 +5,42 @@ import "@fullcalendar/timegrid/main.css";
 import { SessionProvider } from "next-auth/react";
 import Header from "../components/Header";
 import { AnimatePresence } from "framer-motion";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <Header />
-      <AnimatePresence
-        mode="wait"
-        initial={false}
-        onExitComplete={() => window.scrollTo(0, 0)}
-      >
-        <Component {...pageProps} />
-      </AnimatePresence>
-    </SessionProvider>
+    <div>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/img/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/img/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/img/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <SessionProvider session={pageProps.session}>
+        <Header />
+        <AnimatePresence
+          mode="wait"
+          initial={false}
+          onExitComplete={() => window.scrollTo(0, 0)}
+        >
+          <Component {...pageProps} />
+        </AnimatePresence>
+      </SessionProvider>
+    </div>
   );
 }
 
